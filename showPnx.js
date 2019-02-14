1e0a56237783cfc66a22af6ad72d1498b8002e2c
jsicot <julien.sicot@gmail.com>
2019-02-14 13:39:52 +0100

    "added some ws links"

--- showPnx.js
+++ showPnx.js
@@ -19,10 +19,15 @@ javascript:(function() {
      resultItemLastChild = resultItemChildren[resultItemChildren.length-1],
      recordIdSpan = document.createElement("span"),
      hrefBase = showPnxLinkElem[i].querySelector('.media-thumbnail').querySelector('a').getAttribute('ng-href').replace(/docid=.*?(&|$)/, "docid="+showPnxRecId+"$1"),
+     bn = showPnxRecId.replace(/33UDR2_KOHA/, ""),
+     showIntranetLinkHref = "https://pro-koha.bu.univ-rennes2.fr/cgi-bin/koha/catalogue/detail.pl?biblionumber="+bn,
+     showIntranetLink = createLink (showIntranetLinkHref, "Koha pro"),
+     showOAILinkHref = "https://catalogue.bu.univ-rennes2.fr/cgi-bin/koha/oai.pl?verb=GetRecord&metadataPrefix=marcxml&identifier=oai:bu.univ-rennes2.fr:"+bn,
+     showOAILink = createLink (showOAILinkHref, "OAI"),
+     showSRULinkHref = "http://catalogue.bu.univ-rennes2.fr:9998/biblios?version=1.2&operation=searchRetrieve&startRecord=1&maximumRecords=1&query=rec.id%3D"+bn,
+     showSRULink = createLink (showSRULinkHref, "SRU"),
      showSourceLinkHref = hrefBase.replace(/fulldisplay/, "sourceRecord").replace(/docid=/, "docId="),
-     showSourceLink = createLink (showSourceLinkHref, "Unimarc"),
-     showRISLinkHref = "../primo_library/libweb/action/display.do?doc="+showPnxRecId+"&vid="+urlParamVid+"&showRIS=true",
-     showRISLink = createLink (showRISLinkHref, "RIS");
+     showSourceLink = createLink (showSourceLinkHref, "Unimarc");
 
    /* Service Pages via old UI for the time being */
    if (/openurl/.test(location) === true) {
@@ -44,17 +49,13 @@ javascript:(function() {
    /* No Source-links for Third Node (PCI, EBSCO etc.) records */
    if (/context=L/.test(hrefBase) === true) {
      recordIdSpan.appendChild(showSourceLink);
-
-     /* No RIS-links for PrimoVE (no old UI to link to) */
-     if (/\/discovery\//.test(hrefBase) === false) {
-       recordIdSpan.appendChild(showRISLink);
-     }
-
+     recordIdSpan.appendChild(showIntranetLink);
+     recordIdSpan.appendChild(showOAILink);
+     recordIdSpan.appendChild(showSRULink);
    }
 
    /* Append recordid and links only once */
    if (resultItemLastChild.className !== 'show-recordid') {
-
      /* Prepend FRBR records with note */
      if (showPnxLinkElem[i].querySelector('prm-search-result-frbr-line')) {
        var prefix = document.createElement("strong");
